d6af68d29fc04f0374a382d5d81f5ba8aa42d39e
S1mpleKnight <became.adult77@gmail.com>
2020-12-06 18:02:05 +0300

    delete some files

--- src/com/epam/jwd/strategy/Strategy.java
+++
@@ -1,8 +0,0 @@
-package com.epam.jwd.strategy;
-
-import com.epam.jwd.model.Point;
-
-public interface Strategy{
-    double calculateTheArea(Point ... var);
-    double calculateThePerimeter(Point ... var);
-}
